d95b34a4d68d9cc4c7c5b67fab72c0199045c9cd
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2019-12-07 22:52:54 +0200

    First commit

---
+++ first.py
@@ -0,0 +1,92 @@
+# Counter
+# def gen():
+#     counter = 0
+#     while counter <= 10:
+#         print("yield")
+#         print(counter)
+#         yield counter
+#         counter += 1
+#
+# for i in gen():
+#     print("for loop")
+#     print(i)
+
+
+# Decorator
+# import time
+# import random
+#
+# def decorator(func):
+#     def wrapper():
+#         start = time.time()
+#         result = func()
+#         print("It took", time.time() - start, "seconds.")
+#         return result
+#     return wrapper
+#
+# @decorator
+# def add():
+#     time.sleep(random.randint(0, 3))
+#     return 10 + 10
+#
+# @decorator
+# def foo():
+#     return 1
+#
+# print(add())
+
+
+# class Human:
+#
+#     def foo(self):
+#         return 1
+#
+#     def __call__(self, *args, **kwargs):
+#         return "Hello from call"
+#
+# h1 = Human()
+# print(h1.foo())
+# print(h1())
+#
+# def foo():
+#     return 1
+#
+# print(dir(foo))
+
+
+# class Human:
+#     c = 4
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+#
+# h1 = Human("value_a", "value_b")
+# print(h1.a)
+# print(h1.__dict__["a"])
+#
+# print(h1.c)
+# print(h1.__class__.__dict__["c"])
+#
+# h1.d = 4
+# h1.__dict__["d"] = 5
+# print(h1.d)
+#
+# print(h1.__dict__)    # Удобно использовать для просмотра того, что внутри класса.
+
+class Human:
+    def __init__(self, age, skill):
+        self.age = age
+        self.skill = skill
+
+    def __sub__(self, other):
+        return self.age - other.age
+
+    def __add__(self, other):
+        return self.skill + ", " + other.skill
+
+
+young = Human(25, "Testing")
+old = Human(50, "Programming")
+year_difference = old - young
+average_team_skills = old + young
+print(f"Year difference: {year_difference};", f"Average team skills: {average_team_skills}.")
